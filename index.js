96cbd8f968dad93e660148e1e8041786a2e4e4f5
SNSania <150348542+SNSania@users.noreply.github.com>
2024-01-29 23:36:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Please write your statement for word count:"
+    }
+]);
+if (answer.sentence && typeof answer.sentence === 'string') {
+    const words = answer.sentence.trim().split(/\s+/);
+    console.log("Your sentence word count is:", words.length);
+}
